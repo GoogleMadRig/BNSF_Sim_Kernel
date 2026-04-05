77bc8c372b9b84afbd6a29b78c4538eaada4c54a
Daviud Heiblum <dheiblum7252940@gmail.com>
2026-04-05 18:51:24 -0400

    Initial Commit

---
+++ BNSF_Sim_Kernel/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BNSF_Sim_Kernel
+//
+//  Created by David Heiblum on 4/5/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
